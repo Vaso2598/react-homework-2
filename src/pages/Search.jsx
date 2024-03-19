d907feb9d98ba505e632b4f56d804ef821714e76
Vasili <va.chanishvili@gmail.com>
2024-03-19 19:32:05 +0400

    Update search style

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -7,7 +7,7 @@ const Search = () => {
     <div className="flex flex-col bg-teal-950 text-slate-50 gap-8 p-3 rounded-md">
       <section>
         <label>
-          <input type="search" placeholder="What do you want to play?" className="w-full px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-indigo-500"></input>
+          <input type="search" placeholder="What do you want to play?" className="w-full bg-teal-900 px-4 py-2 rounded-full border border-gray-300 focus:outline-none focus:border-indigo-500"></input>
         </label>
       </section>
       <section>
